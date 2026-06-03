cc9926be03df9e0e1f876af9b0806a87f930cdca
Art4v <aarav_20@outlook.com>
2026-06-03 18:15:43 +1000

    added vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import { TopNav } from "@/components/TopNav";
 import { SmoothScrollProvider } from "@/components/SmoothScrollProvider";
 
@@ -9,18 +10,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;0,500;0,600;0,700;1,500;1,600&display=swap" rel="stylesheet" />
-      </head>
-      <body>
-        <SmoothScrollProvider>
-          <TopNav />
-          {children}
-        </SmoothScrollProvider>
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;0,500;0,600;0,700;1,500;1,600&display=swap" rel="stylesheet" />
+        </head>
+        <body>
+          <SmoothScrollProvider>
+            <TopNav />
+            {children}
+          </SmoothScrollProvider>
+        </body>
+      </html>
+      <Analytics />
+    </>
   );
 }
